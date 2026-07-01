9918752a7f5ce9bbcce4fc07135c91079780ca06
e <edarasaitejaswini@gmail.com>
2026-07-02 01:22:03 +0530

    Added Week-2 exercises

---
+++ Week-2/Exercise2_ImplementingDependencyInjection/src/main/java/com/library/Exercise2_ImplementingDependencyInjection/App.java
@@ -0,0 +1,13 @@
+package com.library.Exercise2_ImplementingDependencyInjection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
